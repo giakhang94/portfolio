5e610d41c8ac5ca8f143b674355d21a50cf06253
giakhang94 <ngk.khang94@gmail.com>
2025-06-02 15:52:16 +0700

    change cv link

--- src/constant.ts
+++ src/constant.ts
@@ -1,2 +1,2 @@
 export const CV_LINK =
-  "https://drive.google.com/file/d/1LDfa0W6UsevAvBj3SYJhXODazwzwzf1g/view?usp=sharing";
+  "https://drive.google.com/file/d/1GUpf-aaVq92WD3zP7CrquLz1yuDEQo5b/view?usp=sharing";
